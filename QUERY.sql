38d9ddf5503112788b48f2439d3bc6f3d5f99519
hakimcolor <hakimcolor777@gmail.com>
2026-07-06 22:49:23 +0600

    add the usr data

--- QUERY.sql
+++ QUERY.sql
@@ -13,6 +13,33 @@ DROP TABLE IF EXISTS Users;
 -- =========================================================================
 -- 1. CREATE USERS TABLE
 -- =========================================================================
+
+CREATE TABLE Users (
+    user_id        INTEGER,
+    full_name      VARCHAR(100) NOT NULL,
+    email          VARCHAR(100) NOT NULL,
+    role           VARCHAR(20)  NOT NULL,
+    phone_number   VARCHAR(20),
+
+    CONSTRAINT pk_user_id PRIMARY KEY (user_id),
+
+    CONSTRAINT unique_email UNIQUE (email),
+
+    CONSTRAINT check_role
+        CHECK (
+            role IN ('Ticket Manager', 'Football Fan')
+        )
+);
+
+
+
+
+
+
+
+
+
+
 CREATE TABLE Users (
     user_id TYPE,
     full_name TYPE,
