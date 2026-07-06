b3b1d833a578b155f7e7f9f3e81bfdbe3a33f60a
hakimcolor <hakimcolor777@gmail.com>
2026-07-06 23:14:45 +0600

    5 fatima khan fatimakhangmail

--- QUERY.sql
+++ QUERY.sql
@@ -122,7 +122,8 @@ INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
 (1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
 (2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
-(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL),
+(5, 'Fatima Khan', 'fatima.khan@mail.com', 'Football Fan', '+8801744444444'),
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO MATCHES
