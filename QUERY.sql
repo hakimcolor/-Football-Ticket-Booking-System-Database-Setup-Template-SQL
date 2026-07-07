40018e722c260d09bff43206dffedd44187ed4fa
hakimcolor <hakimcolor777@gmail.com>
2026-07-07 08:13:15 +0600

    find all ticket bookings where the total cost is strictly higher than the average const of all ticket bookings

--- QUERY.sql
+++ QUERY.sql
@@ -149,12 +149,14 @@ WHERE tournament_category = 'Champions League'
   AND match_status = 'Available';
 
 -- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+
 SELECT user_id, full_name, email
 FROM Users
 WHERE full_name LIKE 'Tanvir%'
    OR full_name ILIKE '%Haque%';
 
 -- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+
 SELECT booking_id,
     user_id,
     COALESCE(payment_status, 'Action Required') AS payment_status
@@ -162,6 +164,7 @@ FROM bookings
 WHERE payment_status IS NULL;
 
 -- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+
 SELECT b.booking_id,
        u.full_name,
        m.fixture,
@@ -171,10 +174,16 @@ INNER JOIN Users u ON b.user_id = u.user_id
 INNER JOIN Matches m ON b.match_id = m.match_id;
 
 -- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+
 SELECT u.user_id,
        u.full_name,
        b.booking_id
 FROM Users u
 LEFT JOIN Bookings b ON u.user_id = b.user_id;
 
+
 -- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+
+SELECT booking_id, match_id, total_cost
+FROM Bookings
+WHERE total_cost > (SELECT AVG(total_cost) FROM Bookings);
